7e2ef4d7770bfb66b57b68b8b465c092c99aebbb
Jack Portman <Jacknospampaydaycoin.io>
2018-09-12 23:38:53 +1000

    Add scripts for Upgrade process

---
+++ upgrade_to_60028/dump_privkeys.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+TARBIN=`which tar`
+USER=`whoami`
+CREATEBACKUP=1
+DIR=$PWD
+CURLBIN=`which curl`
+
+source ~/.PayDay/PayDay.conf
+echo "" > privkeys.log
+
+USER=$rpcuser
+PASS=$rpcpassword
+
+if [ "$CURLBIN" == "" ]; then
+        echo "For script working need a curl binary"
+        exit 0
+fi
+
+if [ "$TARBIN" == "" ]; then
+	echo "For script working need a tar binary"
+	exit 0
+fi
+
+pdd -daemon -listen=0 -rpcuser=$USER -rpcpassword=$PASS -enableaccounts -staking=0 -createwalletbackups=100 2>&1
+
+sleep 30
+
+KEYS=`$CURLBIN -s --user $USER:$PASS http://127.0.0.1:7215 --data-binary "{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"listaccounts\", \"params\": [] }" -H "content-type: text/plain" | grep -o "\"[a-zA-Z0-9[:space:]]*\":[0-9\.]*" | tr -d \"`
+
+var1=1
+IFS=$'\n'
+for pair in $KEYS
+do
+key=$(echo $pair | awk -F: '{print $1}')
+[ "$key" == "error" ] || [ "$key" == "result" ] || [ "$key" == "id" ] || {
+
+addr=`$CURLBIN -s --user $USER:$PASS http://127.0.0.1:7215 --data-binary "{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"getaddressesbyaccount\", \"params\": [\"$key\"] }" -H "content-type: text/plain" | grep -o "M[a-zA-Z0-9]*"`
+for ad in $addr
+do
+if [ -n "$ad" ]; then
+priv=`$CURLBIN -s --user $USER:$PASS http://127.0.0.1:7215 --data-binary "{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"dumpprivkey\", \"params\": [\"$ad\"] }" -H "content-type: text/plain" | grep -o "D[a-zA-Z0-9]*"`
+privkeys[$var1]="\"$key\":\"$priv\""
+echo "\"$key\":\"$priv\"" >> privkeys.log
+var1=$(( $var1 + 1 ))
+fi
+done
+}
+done
+pdd stop
+echo "PayDay Server Shutdown."
+exit 0
---
+++ upgrade_to_60028/import_keys.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+TARBIN=`which tar`
+USER=`whoami`
+CREATEBACKUP=1
+DIR=$PWD
+CURLBIN=`which curl`
+
+source ~/.PayDay/PayDay.conf
+
+USER=$rpcuser
+PASS=$rpcpassword
+
+if [ "$CURLBIN" == "" ]; then
+        echo "For script working need a curl binary"
+        exit 0
+fi
+
+if [ "$TARBIN" == "" ]; then
+	echo "For script working need a tar binary"
+	exit 0
+fi
+
+pdd -connect=a.paydaycoin.io:7214 2>&1
+
+PRIVKEYS=`cat privkeys.log`
+
+for data in $PRIVKEYS
+do
+#data=${privkeys[$ind]}
+key=$(echo $data | awk -F: '{print $2}' | tr -d \")
+value=$(echo $data | awk -F: '{print $1}' | tr -d \")
+$CURLBIN -s --user $USER:$PASS http://127.0.0.1:7215 --data-binary "{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"importprivkey\", \"params\": [\"$key\", \"$value\"] }" -H "content-type: text/plain" > /dev/null
+done
+
+# $CURLBIN -s --user $USER:$PASS http://127.0.0.1:7215 --data-binary "{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"stop\", \"params\": [] }" -H "content-type: text/plain" > /dev/null
+
+echo "Wait while PayDay Server downloading blocks and restart, then check balances and Received addresses"
+
+exit 0
